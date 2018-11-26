4b29d59b92a559afe4122edaed3b01e81dd083ec
julesnuggy <julesnuggy@gmail.com>
2018-11-26 21:53:03 +0000

    add basic or placeholder content

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import './App.css';
-import Sidebar from './components/Sidebar';
 import SidebarContainer from './components/SidebarContainer';
 import Navbar from './components/Navbar';
 import {BrowserRouter as Router, Route} from "react-router-dom";
@@ -8,35 +7,55 @@ import Home from './pages/Home';
 import Blog from './pages/Blog';
 import Services from "./pages/Services";
 import Contact from "./pages/Contact";
+import photo from './assets/stevencox.jpg';
+import * as Content from './content/StaticContent';
 
 class App extends Component {
-    render() {
-        return (
-            <Router>
-                <div className="App">
-                    <Navbar/>
-                    <div className="header-bar">
-                        <h1>Steven AJ Cox</h1>
-                        <p>Sharing best practice, ideas and opinions from around the world</p>
-                    </div>
-                    <div className="main-body">
-                      <SidebarContainer>
-                        <Sidebar />
-                      </SidebarContainer>
-                      <Route exact path="/" component={Home}/>
-                      <Route path="/blog" component={Blog}/>
-                      <Route path="/services" component={Services}/>
-                      <Route path="/contact" component={Contact}/>
-                      <SidebarContainer>
-                        <Sidebar/>
-                        <Sidebar/>
-                        <Sidebar/>
-                      </SidebarContainer>
-                    </div>
-                </div>
-            </Router>
-        );
-    }
+  generateParagraphs = content => {
+    return content.map(sentence => <p>{sentence}</p>);
+  };
+
+  render() {
+      return (
+          <Router>
+              <div className="App">
+                  <Navbar/>
+                  <div className="header-bar">
+                      <h1>Steven AJ Cox</h1>
+                      <p>Sharing best practice, ideas and opinions from around the world</p>
+                  </div>
+                  <div className="main-body">
+                    <SidebarContainer>
+                      <Sidebar header="Steven Cox">
+                        <img src={photo} className="sidebar-photo"/>
+                      </Sidebar>
+                      <Sidebar header="Welcome!" style="secondary-text">
+                        {this.generateParagraphs(Content.Welcome)}
+                      </Sidebar>
+                    </SidebarContainer>
+                    <Route exact path="/" component={Home}/>
+                    <Route path="/blog" component={Blog}/>
+                    <Route path="/services" component={Services}/>
+                    <Route path="/contact" component={Contact}/>
+                    <SidebarContainer>
+                      <Sidebar header="Linked Content">
+                        <a href="#">Somewhere</a>
+                        <br />
+                        <a href="#">Over</a>
+                        <br />
+                        <a href="#">The</a>
+                        <br />
+                        <a href="#">Rainbow</a>
+                      </Sidebar>
+                      <Sidebar header="Recent Tweets">
+                        <p>Nothing here yet...</p>
+                      </Sidebar>
+                    </SidebarContainer>
+                  </div>
+              </div>
+          </Router>
+      );
+  }
 }
 
 export default App;
---
+++ src/content/StaticContent.js
@@ -0,0 +1,6 @@
+export const Welcome = [
+  `Welcome to my personal blog. I am currently Fujitsu’s ambassador for Diversity and Inclusion and have a passion for finding organisational best practice, and how learnings from them can be shared.`,
+  `I’ve had an extensive career in the technology sector and I am fascinated by how technological advances in automation and artificial intelligence impact the modern workforce and in particular what it means for increasing workplace inclusion and workforce diversity.`,
+  `I’m an executive sponsor of Shine, Fujitsu’s LGBT+ and ally networking group. In 2017 Fujitsu UK was ranked number 48 in Stonewall’s equality index as a result of its work around inclusion.`,
+  `I have featured on the OUTstanding/Financial Times LGBT top executives list and The Times top 50 LGBT executives in Business List. I am proud to be a Stonewall Ambassador.`
+];
