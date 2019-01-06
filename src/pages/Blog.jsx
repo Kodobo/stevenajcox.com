ba8bb19b34ffd4daa7d394ba89a96cb0dd852587
julesnuggy <julesnuggy@gmail.com>
2019-01-06 14:55:31 +0000

    feat: add Loading icon to Blog page

--- src/pages/Blog.jsx
+++ src/pages/Blog.jsx
@@ -1,25 +1,48 @@
 import React, { Component } from 'react';
 import { getPosts } from '../apis/bloggerApi';
 import ReactHtmlParser from 'react-html-parser';
+import Loader from 'react-loader-spinner';
+import './Blog.css';
 
 export default class Blog extends Component {
   constructor() {
     super();
     this.state = {
+      loading: true,
       bloggerPosts: []
     }
   }
   componentDidMount() {
-    Promise.resolve(getPosts()).then(res => {
-      this.setState({bloggerPosts: res})
-    });
+    Promise.resolve(getPosts())
+      .then(res => {
+        this.setState({bloggerPosts: res})
+      })
+      .then(() => {
+        this.setState({loading: false});
+      })
   }
+
+  renderBlogPage = () => {
+    const { loading } = this.state;
+
+    return loading ? this.renderLoadingIcon() : this.renderPosts();
+  };
+
+  renderLoadingIcon = () => {
+    return (
+      <div className="LoadingContainer">
+        <Loader type="CradleLoader"
+                height="100"
+                width="100" />
+      </div>
+    );
+  };
 
   renderPosts = () => {
     const { bloggerPosts } = this.state;
     return bloggerPosts.map(post => {
       return ReactHtmlParser(post);
-    })
+    });
   };
 
   render() {
@@ -28,7 +51,7 @@ export default class Blog extends Component {
         <h1>
             Blog
         </h1>
-        {this.renderPosts()}
+        {this.renderBlogPage()}
       </div>
     )
   }
