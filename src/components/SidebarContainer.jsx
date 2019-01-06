5b596e8d39b9b18a233b3c451a3563ffb051dd6b
Lee Stewart <luigizan88@gmail.com>
2019-01-06 13:27:07 +0000

    add embedded twitter feed to right sidebar

--- src/components/SidebarContainer.jsx
+++ src/components/SidebarContainer.jsx
@@ -4,7 +4,7 @@ import './SidebarContainer.css';
 export default class SidebarContainer extends Component {
   render() {
     return (
-      <div class="sidebar-container">
+      <div className="sidebar-container">
         {this.props.children}
       </div>
     )
