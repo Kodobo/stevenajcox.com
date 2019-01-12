9952ad1a723b18494b9810607414f2b439327c9c
julesnuggy <julesnuggy@gmail.com>
2019-01-12 14:23:15 +0000

    Add page card style

--- src/components/styles/BloggerPosts.js
+++ src/components/styles/BloggerPosts.js
@@ -1,5 +1,16 @@
 import styled from 'styled-components';
+import { COLOURS } from "../../styleConstants/colours";
 
+// The descendant selectors need to use !important to override the
+// styles hardcoded into the Blogger post tags.
 export const BlogPostContainer = styled.div`
   margin: 20px 0;
+  
+  & * {
+    background-color: ${COLOURS.LIGHT_GREY} !important;
+  }
+  
+  & .MsoTableGrid {
+    width: 100% !important;
+  }
 `;
--- src/pages/styles/Pages.js
+++ src/pages/styles/Pages.js
@@ -1,7 +1,11 @@
 import styled from 'styled-components';
+import { COLOURS } from "../../styleConstants/colours";
 
 export const BodyContent = styled.div`
   width: 100%;
-  padding-top: 25px;
+  height: 100%;
+  padding: 15px;
   margin: 50px;
+  border-radius: 4px; 
+  background-color: ${COLOURS.LIGHT_GREY};
 `;
