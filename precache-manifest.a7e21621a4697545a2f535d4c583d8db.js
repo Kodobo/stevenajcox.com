bb3df7cb27dbd337a036e089dac550872ae74967
Lee Stewart <luigizan88@gmail.com>
2019-01-19 16:58:39 +0000

    Updates

---
+++ precache-manifest.a7e21621a4697545a2f535d4c583d8db.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "3815c4e39e77bf213763efa53063115e",
+    "url": "/stevenajcox.com/static/media/stevencox.3815c4e3.jpg"
+  },
+  {
+    "revision": "88c6c34d015cdb3c0101",
+    "url": "/stevenajcox.com/static/js/runtime~main.88c6c34d.js"
+  },
+  {
+    "revision": "478764c083efcd0988d0",
+    "url": "/stevenajcox.com/static/js/main.478764c0.chunk.js"
+  },
+  {
+    "revision": "022379abbc38f2389f31",
+    "url": "/stevenajcox.com/static/js/1.022379ab.chunk.js"
+  },
+  {
+    "revision": "478764c083efcd0988d0",
+    "url": "/stevenajcox.com/static/css/main.c0d63a9f.chunk.css"
+  },
+  {
+    "revision": "022379abbc38f2389f31",
+    "url": "/stevenajcox.com/static/css/1.4dab16ea.chunk.css"
+  },
+  {
+    "revision": "53ceb4fc10669eab480a92c94a0ff050",
+    "url": "/stevenajcox.com/index.html"
+  }
+];
